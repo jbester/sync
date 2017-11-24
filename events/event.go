0a4dac391d731c467f8e979cabe3b36af04fa704
Jeffrey Bester <jbester@gmail.com>
2017-11-23 21:23:41 -0500

    Updated doc comments

--- events/event.go
+++ events/event.go
@@ -13,13 +13,13 @@
 // OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Package events provides a single synchronization primitive the Event.  The event is used to notify the occurrence of
-// a condition to routines.  Each event has two states - up and down.  Up indicates the condition has occurred.
+// Package events provides a single synchronization primitive - the Event.  The event is used to notify that
+// a condition has occurred to routines.  Each event has two states - set and unset.  Set state indicates the condition has occurred.
 //
-// Multiple routines can wait on a condition.   All routines unblock once the condition occurs.
-// A routine that waits on a condition that has already occurred will not block.
+// Multiple routines can wait on a condition.   _All_ routines unblock once the event is set to the set state.
+// A routine that waits on an event that is already set will not block.
 //
-// The event primitive is similar to the event in the pSOS or ARINC 653 API sets.
+// The event primitive is similar to the event in the pSOS or ARINC 653 APIs.
 package events
 
 import (
@@ -33,13 +33,14 @@ type Event struct {
 	notifyList *startgroup.StartGroup
 }
 
-// Creates an event object for use by any routine.  Upon creation the event is set to the down state.
+// Creates an event object for use by any routine.  Upon creation the event is set to the unset state.
 func MakeEvent() *Event {
 	return &Event{state: 0, notifyList: startgroup.MakeStartGroup()}
 }
 
-//  Set request sets the specified event to the up state. All the routines waiting
-//  on the event will stop waiting.  Any routine attempting to wait will not block.
+//  Set changes the specified event to the set state. All the routines waiting
+//  on the event will stop waiting.  Any subsequent routine attempting to wait will not block until the
+//  event is set to the unset state again.
 //  Returns true if the event changed.
 func (evt *Event) Set() bool {
 	if atomic.CompareAndSwapInt32(&evt.state, 0, 1) {
@@ -49,19 +50,19 @@ func (evt *Event) Set() bool {
 	return false
 }
 
-//  Checks if the specified event is in up state.
+//  Checks if the specified event is in set state.
 func (evt *Event) IsSet() bool {
 	return atomic.LoadInt32(&evt.state) == 1
 }
 
-//  Resets the specified event to the down state.   Once reset any routine attempting to wait
+//  Resets the specified event to the unset state.   Once reset, any routine attempting to wait
 //  will block until the event is set again.  Returns true if the event changed.
 func (evt *Event) Reset() bool {
 	return atomic.CompareAndSwapInt32(&evt.state, 1, 0)
 }
 
-//  Wait for the event to be in the up state.  Any routine that attemps to wait on an event
-//  already in the up state will not block.
+//  Wait for the event to be in the set state.  Any routine that attempts to wait on an event
+//  already in the set state will not block.
 func (evt *Event) Wait() {
 	if evt.IsSet() {
 		return
@@ -70,8 +71,8 @@ func (evt *Event) Wait() {
 	}
 }
 
-//  Wait for the vent to be in the up state.  Any routine waiting on an event
-//  in the up state will not block.   Returns true if the event is in the up state and
+//  Wait for the event to be in the set state.  Any routine waiting on an event
+//  in the set state will not block.   Returns true if the event is in the set state and
 //  false if a timeout occurred.
 func (evt *Event) TryWait(timeout time.Duration) bool {
 	var ok bool
